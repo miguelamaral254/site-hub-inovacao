77c4574e9719c3b05e39b1e9e4fae08c01949538
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2025-01-30 17:48:49 -0300

    correção de redirecionamento

--- hub-inovacao/src/app/inicio/page.tsx
+++ hub-inovacao/src/app/inicio/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import Navbar from "@/components/Navbar";
+
 export default function Inicio() {
   return (
     <div>
+      <Navbar/>
       <h1> porraaaaaa</h1>
     </div>
   );
