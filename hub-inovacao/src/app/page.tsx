61fec5331485747c2c688dc3b84bdb46d65c549d
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2025-01-30 17:19:12 -0300

    navbar com erro de rota

--- hub-inovacao/src/app/page.tsx
+++ hub-inovacao/src/app/page.tsx
@@ -1,9 +1,9 @@
-import LoginPage from "@/features/signin/LoginPage";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <LoginPage/>
+      <Navbar/>
           </div>
   );
 }
---
+++ hub-inovacao/src/features/inicio/InicioPage.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Inicio() {
+  return (
+    <div>
+      <h1> porraaaaaa</h1>
+    </div>
+  );
+}
