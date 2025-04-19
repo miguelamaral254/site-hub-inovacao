a806f46788f15180c8c708f1bb6843b0ab88fa67
miguelamaral254 <miguel.amaral.sales@gmail.com>
2025-04-19 01:47:07 -0300

    finalizado tickets para projetos

--- hub-inovacao/src/features/core/page.interface.ts
+++ hub-inovacao/src/features/core/page.interface.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Page<T> {
+    data: Page<import("/Users/miguelamaral/Documents/dev/site-hub-inovacao/hub-inovacao/src/features/projects/project.interface").Project>;
+    page: any;
     content: T[];
     totalElements: number;
     totalPages: number;
